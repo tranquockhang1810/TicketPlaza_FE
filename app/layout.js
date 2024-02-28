b7c4db2911c347010541e548c2adb84586c771fc
TranQuocKhang1810 <tranquockhang1810@gmail.com>
2024-02-28 08:39:53 +0700

    Initial soures

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import "./global.scss";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -10,8 +8,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="vi">
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import { Button } from 'antd';
+import '../app/layout'
+
+export default function Page() {
+  return (
+    <div className='flex justify-center'>
+      <p 
+        className='m-2'
+      >
+        XIN CHÀO BÉ IU
+      </p>
+      <Button
+        className='button-test m-2'
+      >
+        Nút
+      </Button>
+    </div>
+)}
